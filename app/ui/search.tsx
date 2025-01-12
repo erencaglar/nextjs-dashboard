eb3c5b0e4e5863e7260b207cb784916bae270a73
erencaglar <ern.edn.42@gmail.com>
2025-01-12 17:24:21 +0300

    chapter 11 pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,10 +10,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const {replace} = useRouter();
 
   const handleSearch = useDebouncedCallback((term:string) =>{
-    console.log(`Searching... ${term}`);
-
     // Handle search
-    const params = new URLSearchParams(searchParams);
+   
+    const params = new URLSearchParams(searchParams); 
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else{
